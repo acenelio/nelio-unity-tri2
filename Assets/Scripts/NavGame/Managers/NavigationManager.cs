0c942e147bf5b228c9cde5794885569462051afa
DevSuperior <devsuperior.contato@gmail.com>
2020-06-26 15:18:55 -0300

    Navigation

---
+++ Assets/Scripts/NavGame/Managers/NavigationManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NavigationManager : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
