750cea0b8e01bfab56828c0805c0ac534b268982
DevSuperior <devsuperior.contato@gmail.com>
2020-06-28 11:22:54 -0300

    Navigation

---
+++ Assets/Scripts/NavGame/Managers/NavigationManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class NavigationManager : MonoBehaviour
+{
+    public static NavigationManager instance;
+ 
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+ 
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+ 
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
